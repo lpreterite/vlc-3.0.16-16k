2feda6d7d5cc48d3f51b8e2506c97e95fb59c652
Packy <lpreterite@126.com>
2021-08-13 10:52:04 +0800

    将视频限制从8K增加至16K

--- modules/codec/avcodec/video.c
+++ modules/codec/avcodec/video.c
@@ -155,7 +155,7 @@ static int lavc_GetVideoFormat(decoder_t *dec, video_format_t *restrict fmt,
     else /* hardware decoding */
         fmt->i_chroma = vlc_va_GetChroma(pix_fmt, sw_pix_fmt);
 
-    if( width == 0 || height == 0 || width > 8192 || height > 8192 ||
+    if( width == 0 || height == 0 || width > 16384 || height > 16384 ||
         width < ctx->width || height < ctx->height )
     {
         msg_Err(dec, "Invalid frame size %dx%d vsz %dx%d",
